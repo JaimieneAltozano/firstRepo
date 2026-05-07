b31a9a5a027b9791d4802ce4b7cfe0c92e2ccd4a
JaimieneAltozano <violybeatriz2638@gmail.com>
2026-05-07 11:07:15 -0500

    Add files via upload
    qwq

--- pio/M3/week1/interactive_system.js
+++ pio/M3/week1/interactive_system.js
@@ -1,30 +1,14 @@
+function register(name, age) {
+    let status = (age >= 18) ? "compra un ataúd" : "¿será que aún cabes en la cuna?";
+    alert(`${name}, ${age}, ${status}`);
+}
+
 const name = prompt("Enter ur name");
 let age = parseInt(prompt("Your age"));
 
-let status = (age >= 18) ? "compra un ataúd" : "¿será que aún cabes en la cuna?";
-
-alert(`${name}, ${status}`);
-
-
-//else{
-//    alert("Try again");
-//}
-
-let userConsent = confirm("Do u want to sleep?");
-
-if (userConsent) {
-    alert("Pay me first");
-}else{
-    alert("Why you didn't want to marry with the computer?");
+while(isNaN(age)) {
+    alert("Try again");
+    age = parseInt(prompt("Your age"));
 }
 
-for (let i = 0; i < 5; i++) {
-    alert(i);
-}
-
-let i = 4;
-
-while (i > -1) {
-    alert(i);
-    i--;
-}
+register(name, age);
