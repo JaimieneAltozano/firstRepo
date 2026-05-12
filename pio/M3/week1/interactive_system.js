af69165ebd921cb199034546fd9bf58b1155983f
JaimieneAltozano <violybeatriz2638@gmail.com>
2026-05-12 11:02:31 -0500

    Add files via upload
    pio

--- pio/M3/week1/interactive_system.js
+++ pio/M3/week1/interactive_system.js
@@ -1,10 +1,10 @@
-function register(name, age) {
-    let status = (age >= 18) ? "compra un ataúd" : "¿será que aún cabes en la cuna?";
-    alert(`${name}, ${age}, ${status}`);
-}
-
 const name = prompt("Enter ur name");
 let age = parseInt(prompt("Your age"));
+
+function register(name, age) {
+    let status = (age >= 18) ? "compra un ataúd" : "¿será que aún cabes en la cuna?";
+    alert(`${name}, tienes ${age} años, ${status}`);
+}
 
 while(isNaN(age)) {
     alert("Try again");
