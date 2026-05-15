1bf9820a9b0bd2426d095a0f9e99e0448df6806a
JaimieneAltozano <violybeatriz2638@gmail.com>
2026-05-15 09:44:54 -0500

    Add files via upload
    :D

--- pio/M3/week2/gestion_datos.js
+++ pio/M3/week2/gestion_datos.js
@@ -1,17 +1,6 @@
 // Create product objects
-const products = [];
-const product1 = [];
-const product2 = [];
-
-products[product1 [{
-    "id": 1,
-    "name": "Pato",
-    "price": 12
-}], product2 [{
-    "id": 2,
-    "name": "Pollito",
-    "price": 11
-}]];
+const product1 = {id: '1', name: 'Pato', price: 12};
+const product2 = {id: '2', name: 'Pollito', price: 11};
 
 // "Set" practice ;D
 const nums = new Set([1, 1, 1, 2, 3, 3, 4, 5, 5]);
@@ -32,3 +21,11 @@ for (const i of nums) {
 const deleted = document.getElementById("deleted").textContent = "A number has been deleted " + count; // Here could had a duplication but "count" was reset
 
 // "Map" practice
+const productos = new Map();
+
+productos.set(product1, {color: 'Yellow'});
+productos.set(product2, {color: 'Pink'});
+
+alert(Object.keys(product1)); // Mostrar clave
+alert(Object.values(product1)); // Mostrar valor
+alert(JSON.stringify(product1)); // Mostrar ambos :D
